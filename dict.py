cd4fe549390af75cc3aefc0000dfc2e9a6499a71
QuickPlayz <94027748+QuickPlayz@users.noreply.github.com>
2021-11-18 07:57:40 -0500

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,29 @@
+thisdict = {
+    
+}
+
+print('Cars Brand?')
+A = input()
+print('Cars Model?')
+B = input()
+print('Cars Year?')
+C = input()
+print('Cars Color?')
+D = input()
+print('Cars Condition?')
+E = input()
+print('Cars Owner?')
+F = input()
+print('Cars Licence Plate?')
+G = input()
+
+
+thisdict["Brand"] = A
+thisdict["Model"] = B
+thisdict["Year"] = C
+thisdict["Color"] = D
+thisdict["Condition"] = E
+thisdict["Owner"] = F
+thisdict["Plate"] = G
+
+print(thisdict)
